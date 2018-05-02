6cfb033325bd61f12d1aa63f25ba9eb83eb6d0ff
Luis Gonzalez <luisal1207@hotmail.es>
2018-05-02 15:07:08 -0600

    Update the deploying file with a variable to store the promise.

--- .deploy.js
+++ .deploy.js
@@ -2,7 +2,7 @@ console.log("Starting")
 let ghpages = require("gh-pages")
 
 console.log("Publishing the project to gh-pages branch")
-ghpages.publish("dist", {
+let thepromise = ghpages.publish("dist", {
   branch: "gh-pages"
 }, () => {
   console.log("Updated!")
