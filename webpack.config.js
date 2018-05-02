653bd0efe77e864e781c55c6e17a62c36f0ce737
Luis Gonzalez <luisal1207@hotmail.es>
2018-05-02 13:13:07 -0600

    Provisional commit

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,9 @@ module.exports = {
         new BrowserSyncPlugin({
             host: 'localhost',
             port: 3000,
-            server: { baseDir: ['dist'] },
+            server: {
+                baseDir: ['dist']
+            },
             files: ['./dist/*']
         }),
     ],
